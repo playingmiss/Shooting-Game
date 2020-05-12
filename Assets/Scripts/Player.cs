b67479f90464cc6cfb5c3a5c8ceaa8305a4a60b5
Naga Tomohiro <to.1101.tennis.com@outlook.jp>
2020-05-12 15:51:57 +0900

    ドラッグ&ドロップ

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+public void OnDrag()
+    {
+        Vector3 TapPos = Input.mousePosition;
+        TapPos.z = 10f;
+        transform.position = Camera.main.ScreenToWorldPoint(TapPos);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
