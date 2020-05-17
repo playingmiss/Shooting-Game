7aa4e35d90a38572bab1eb5372e661790ca3175e
嶋川　晋 <>
2020-05-17 13:50:30 +0900

    弾の発射と削除

---
+++ Assets/Scripts/BulletManegar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletManegar : MonoBehaviour
+{
+    
+    
+
+    // Update is called once per frame
+    void Update()
+    {   //弾の移動
+        transform.position += new Vector3(0,0.1f,0);
+
+        //弾の削除
+        if (transform.position.y > 5) {
+			Destroy (gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,9 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+// PlayerBullet プレハブ
+public GameObject bullet;
+
 public void OnDrag()
     {
         Vector3 TapPos = Input.mousePosition;
@@ -15,12 +18,25 @@ public void OnDrag()
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine("Shot");
+        //弾の発射処理（コルーチン Shot ）を実行
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+    
+    //たまの発射処理
+    IEnumerator Shot()
+    {
+        while (true)
+        {
+            // 弾をプレイヤーと同じ位置/角度で作成
+            Instantiate(bullet, transform.position, transform.rotation);
+            // 0.05秒待つ
+            yield return new WaitForSeconds(0.2f);
+        }
     }
 }
