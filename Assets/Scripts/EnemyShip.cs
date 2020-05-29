5db6edf2acfb5f551a1620538f432835efd29e5d
嶋川　晋 <>
2020-05-29 16:03:30 +0900

    ゲームオーバーの実装

--- Assets/Scripts/EnemyShip.cs
+++ Assets/Scripts/EnemyShip.cs
@@ -8,6 +8,8 @@ public class EnemyShip : MonoBehaviour
 {
     public ScoreCounter gamecontroller;
     public GameObject explosion;//破壊のプレふぁぶ
+    public GameObject explosion2;//自機の爆発
+
     //public AudioClip sound1;
     //AudioSource audioSource;
     // Start is called before the first frame update
@@ -41,7 +43,9 @@ public class EnemyShip : MonoBehaviour
         }
 
         if (collision.gameObject.tag == "player"){
+            Instantiate(explosion2,transform.position,transform.rotation);
             Debug.Log("Game Over");
+            gamecontroller.GameOver();
             Destroy(collision.gameObject);
         }
     }
--- Assets/Scripts/ScoreCounter.cs
+++ Assets/Scripts/ScoreCounter.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 public class ScoreCounter : MonoBehaviour
 {
+    public GameObject GameOverText;
     public Text scoreText ;
     int score = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+      GameOverText.SetActive(false);  
       scoreText.text = "SCORE:" + score;  
     }
 
@@ -18,8 +22,13 @@ public class ScoreCounter : MonoBehaviour
         scoreText.text = "SCORE:" + score;
     }
     // Update is called once per frame
+    public void GameOver(){
+        GameOverText.SetActive(true);
+    }
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space)){
+            SceneManager.LoadScene("unity_shoot_22");
+        }
     }
 }
