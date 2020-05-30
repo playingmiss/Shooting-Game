59845b7a15c4da14524997754f16c60e073e2201
Naga Tomohiro <to.1101.tennis.com@outlook.jp>
2020-05-30 10:08:50 +0900

    スクロールを改善

--- Assets/Scripts/BackgroundController.cs
+++ Assets/Scripts/BackgroundController.cs
@@ -13,9 +13,9 @@ public class BackgroundController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate (0, -0.09f, 0);
-		if (transform.position.y < -4.9f) {
-			transform.position = new Vector3 (0, 4.9f, 0);
+        transform.Translate (0, -0.1f, 0);
+		if (transform.position.y <= -15.8f) {
+			transform.position = new Vector3 (0, 48.9f, 0);
         }
     }
 }
